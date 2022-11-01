65ae30f75fcba034cc44d9765ea6973ad7890b24
Danny Steinbrecher <danny.steinbrecher@fh-erfurt.de>
2022-11-01 14:09:24 +0100

    deleted booking service, because I will work out a good basis from the cinema service in the first place.

--- bookingservice/src/main/kotlin/de/keksbendiger/moviesservice/models/Properties.kt
+++
@@ -1,8 +0,0 @@
-package de.keksbendiger.moviesservice.models
-
-class Properties(
-    private val msg: String,
-    private val buildVersion: String,
-    private val mailDetails: Map<String, String>,
-    private val activeBranches: List<String>
-)
