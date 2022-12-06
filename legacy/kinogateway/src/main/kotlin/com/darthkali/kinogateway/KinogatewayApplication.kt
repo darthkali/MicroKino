1fb74a7b0ce2d8e14bf37bc1139769a952db7f96
Keksbendiger <keksbendiger@gmail.com>
2022-12-06 10:39:57 +0100

    moved unused files to 'legacy'

---
+++ legacy/kinogateway/src/main/kotlin/com/darthkali/kinogateway/KinogatewayApplication.kt
@@ -0,0 +1,17 @@
+package com.darthkali.kinogateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class KinogatewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<KinogatewayApplication>(*args)
+}
+
+
+
+
+
